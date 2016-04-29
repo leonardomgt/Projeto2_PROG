dac4fae3fac64a94636f02ecc9ee0652cca81d58
leonardomgt <leogt-15@hotmail.com>
2016-04-29 14:10:43 +0100

    Update Date.h

--- Date.h
+++ Date.h
@@ -7,16 +7,16 @@ public:
 	void setDay(unsigned int y);
 	void setMonth(unsigned int h); 
 	void setYear(unsigned int d);
-	void setHour(unsigned int h);
-	void setMinute(unsigned int m);
-	void setSecond(unsigned int s);
+	void setHours(unsigned int h);
+	void setMinutes(unsigned int m);
+	void setSeconds(unsigned int s);
 	void setDate(unsigned int day, unsigned int month, unsigned int year, unsigned int hour, unsigned int min, unsigned int sec);
 	unsigned int getDay(unsigned int y);
 	unsigned int getMonth(unsigned int h);
 	unsigned int getYear(unsigned int d);
-	unsigned int getHour(unsigned int h);
-	unsigned int getMinute(unsigned int m);
-	unsigned int getSecond(unsigned int s);
+	unsigned int getHours(unsigned int h);
+	unsigned int getMinutes(unsigned int m);
+	unsigned int getSeconds(unsigned int s);
 	unsigned int getDate(unsigned int day, unsigned int month, unsigned int year, unsigned int hour, unsigned int min, unsigned int sec);
 	~Date();
 
@@ -24,7 +24,7 @@ private:
 	unsigned int day; 
 	unsigned int month;
 	unsigned int year;
-	unsigned int hour;
-	unsigned int minute;
-	unsigned int second;
+	unsigned int hours;
+	unsigned int minutes;
+	unsigned int seconds;
 };
