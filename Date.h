b91a30308e607d2d65115150f151511504f24270
joaopedrofump <joaopedrofump@gmail.com>
2016-05-10 17:00:55 +0100

    Update Date.h

--- Date.h
+++ Date.h
@@ -1,30 +1,35 @@
-class Date
-{
-public:
-	Date();
-	Date(unsigned int day, unsigned int month, unsigned int year, unsigned int hour, unsigned int min, unsigned int sec);
-	Date(string DMY_hms);
-	void setDay(unsigned int y);
-	void setMonth(unsigned int h); 
-	void setYear(unsigned int d);
-	void setHours(unsigned int h);
-	void setMinutes(unsigned int m);
-	void setSeconds(unsigned int s);
-	void setDate(unsigned int day, unsigned int month, unsigned int year, unsigned int hour, unsigned int min, unsigned int sec);
-	unsigned int getDay(unsigned int y);
-	unsigned int getMonth(unsigned int h);
-	unsigned int getYear(unsigned int d);
-	unsigned int getHours(unsigned int h);
-	unsigned int getMinutes(unsigned int m);
-	unsigned int getSeconds(unsigned int s);
-	unsigned int getDate(unsigned int day, unsigned int month, unsigned int year, unsigned int hour, unsigned int min, unsigned int sec);
-	~Date();
+# pragma once
 
-private:
-	unsigned int day; 
-	unsigned int month;
-	unsigned int year;
-	unsigned int hours;
-	unsigned int minutes;
-	unsigned int seconds;
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+class Data {
+ private:
+    int dia;
+    int mes;
+    int ano;
+    bool valid;
+    bool isLeap() const;
+    int numDays() const;
+    bool isValid();
+    
+ public:
+    Data(){};
+    Data(string data); // data na forma DD/MM/AAA
+    Data(unsigned int dia, unsigned int mes, unsigned int ano);
+    int getDia() const;
+    int getMes() const;
+    int getAno() const;
+    bool getValid() const;
+    void setDia(int dia);
+    void setMes(int mes);
+    void setAno(int ano);
+    void save(ofstream &out) const;
+    friend bool operator>=(const Data &date1, const Data date2);
+    friend ostream& operator<<(ostream& out, const Data & data);
 };
