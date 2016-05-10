35eefd00c417bf90315f38dd7718b388a355f9d4
joaopedrofump <joaopedrofump@gmail.com>
2016-05-10 18:21:30 +0100

    Add files via upload

---
+++ utils.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+#include "defs.h"
+using namespace std;
+
+void clearScreen();
+
+unsigned short int leUnsignedShortInt(unsigned short int min, unsigned short int max);
+
+int leInteiro(int min, int max);
+
+void trimString(string &inputString);
